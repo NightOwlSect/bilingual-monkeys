ac765d98532d9a555a970655656a288f47578ff7
NightOwlSect <doyouroshi@gmail.com>
2026-06-16 18:02:19 +0000

    FUCCCCCCKKK

--- list_methods.py
+++ list_methods.py
@@ -2,6 +2,7 @@ shopping_list = []
 action = ""
 while action != "stop":
     elemnt = input("Type the object you want to add to the list: ")
+    shopping_list.append(elemnt)
     # add code to add the entered item here
     action = input("Type 'clear' to clear the list or 'stop' to stop entry: ")
     # add code to clear the list if the user typed clear here
